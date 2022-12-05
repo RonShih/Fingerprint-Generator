1090a59eefa753dbebb4e2cc247011390a0bd144
Ron_YCS <tai54331@gmail.com>
2022-12-05 23:15:59 +0800

    Update
    Correct output form

--- hash.py
+++ hash.py
@@ -2,42 +2,33 @@ import glob
 import hashlib
 import sys
 import os
-CHUNKSIZE = 4096 # a chunk size
-count = 0 #total file count
-total_size_in_BYTE = 0 #total size of files
-unique_size_in_BYTE = 0 #total files size without duplicated data chunk
+CHUNKSIZE = 512 # a chunk size
+total_chunks_num = 0 
+unique_chunks_num = 0 
 
-def num_of_file(): #calculate the total file number
-    count = 0
-    for filename in glob.glob("input/*"):
-        count += 1
-    return count
+def show_dedupe_rate(total_chunks_num, unique_chunks_num): #calculate dedupe_rate
+    print ('Num of chunks                  = ', total_chunks_num)
+    print ('Num of duplicate chunks        = ', total_chunks_num - unique_chunks_num)
+    print ('Dedupe rate can be achieved to = ', (1.0 - (float(unique_chunks_num)/total_chunks_num))*100, '%')
 
-def show_dedupe_rate(total_size_in_BYTE, unique_size_in_BYTE): #calculate dedupe_rate
-    print ('total size         = ', total_size_in_BYTE)
-    print ('not duplicate size = ', unique_size_in_BYTE)
-    print ('duplicate size     = ', total_size_in_BYTE-unique_size_in_BYTE)
-    print ('non duplicate rate = ', float(unique_size_in_BYTE)/total_size_in_BYTE)
-    print ('dedupe_rate        = ', (1.0 - float(unique_size_in_BYTE)/total_size_in_BYTE))
-
-output_file = open('output_fp.txt', 'w')
+output_file = open('/mnt/c/Users/Ron/Desktop/output.txt', 'w')
 
 hash_table = dict() #hash table (using dictionary)
-count = num_of_file()
 
-for filename in glob.glob('input/*'):
-    print('Test: ', filename, 'with chunk size', CHUNKSIZE)
+for filename in glob.glob("/mnt/c/Users/Ron/Desktop/Input/*"): #input path
+    print('Test:', filename, 'with chunk size', CHUNKSIZE)
     with open(filename, 'rb') as afile: #open each file
-        for cur_chunk in iter(lambda: afile.read(CHUNKSIZE), b''):
-            hasher = hashlib.sha1(cur_chunk) #hash the file
-            fingerprint = hasher.hexdigest() #fingerprint is a hash value
+        for cur_chunk in iter(lambda: afile.read(CHUNKSIZE), b''): #for each chunk
+            hasher = hashlib.sha1(cur_chunk) #hash the chunk
+            fingerprint = hasher.hexdigest()
             check_duplicate = fingerprint in hash_table #check if this fingerprint in hash_table
             if(check_duplicate == False):
                 hash_table[fingerprint] = cur_chunk #insert it into dictionary
-                unique_size_in_BYTE += len(cur_chunk)
-            total_size_in_BYTE += len(cur_chunk)
+                unique_chunks_num += 1
+            # else this is a duplicate chunk
+            total_chunks_num += 1
             output_file.writelines(fingerprint + '\n') #write fp into output file
             afile.seek(CHUNKSIZE, 1) #move to next chunk
 
-show_dedupe_rate(total_size_in_BYTE, unique_size_in_BYTE)
+show_dedupe_rate(total_chunks_num, unique_chunks_num)
 
