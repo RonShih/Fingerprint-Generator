3197ac70ff193994a85745f71b7b1b1688c7c42b
Ron_YCS <tai54331@gmail.com>
2022-12-05 15:27:19 +0800

    HUGE update
    1. Modified shitty code I wrote many years ago
    2. Write all fingerprints into an output file for future use

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-## Target
-This program derives deduplication rate of a file in python
+## Goal
+This program derives deduplication rate of a file in python and get fingerprints output for future use.
 
 ## Steps in this program:
-1. Reading a file
-2. Chunking in fixed-size method (4KB in example) 
+1. Reading files in a folder
+2. For each file it chunks in fixed-size method (4KB in example) 
 3. Building a hash table
 4. Hashing each chunk and insert its fingerprint into this hash table
+5. Write its fingerprint into output file
 5. Derive dedup rate
--- hash.py
+++ hash.py
@@ -2,57 +2,42 @@ import glob
 import hashlib
 import sys
 import os
-BLOCKSIZE = 4096 # a file size
+CHUNKSIZE = 4096 # a chunk size
 count = 0 #total file count
-total = 0 #total files size
-unique = 0 #total files size without duplicated data chunk
+total_size_in_BYTE = 0 #total size of files
+unique_size_in_BYTE = 0 #total files size without duplicated data chunk
 
 def num_of_file(): #calculate the total file number
     count = 0
-    for filename in glob.glob('bsplit.*'):
-        if(filename != 'bsplit.c' and filename != 'bsplit.mk' and filename != 'bsplit.o'):
-            count += 1
+    for filename in glob.glob("input/*"):
+        count += 1
     return count
 
-def show_dedupe_rate(total, unique): #calculate dedupe_rate
-    print 'total size         = ', total
-    print 'not duplicate size = ', unique
-    print 'duplicate size     = ', total-unique
-    print 'non duplicate rate = ', float(unique)/total
-    print 'dedupe_rate        = ', (1.0 - float(unique)/total)
+def show_dedupe_rate(total_size_in_BYTE, unique_size_in_BYTE): #calculate dedupe_rate
+    print ('total size         = ', total_size_in_BYTE)
+    print ('not duplicate size = ', unique_size_in_BYTE)
+    print ('duplicate size     = ', total_size_in_BYTE-unique_size_in_BYTE)
+    print ('non duplicate rate = ', float(unique_size_in_BYTE)/total_size_in_BYTE)
+    print ('dedupe_rate        = ', (1.0 - float(unique_size_in_BYTE)/total_size_in_BYTE))
 
-
+output_file = open('output_fp.txt', 'w')
 
 hash_table = dict() #hash table (using dictionary)
 count = num_of_file()
-filename = 'Small_file.dms'
 
-for filename in glob.glob('Small_file*.dms'):
-    print 'Test: ', filename, ' with blocksize ', BLOCKSIZE
+for filename in glob.glob('input/*'):
+    print('Test: ', filename, 'with chunk size', CHUNKSIZE)
     with open(filename, 'rb') as afile: #open each file
-                buf = afile.read(BLOCKSIZE) #read each file
-                count = 0
-                while(len(buf) > 0):
+        for cur_chunk in iter(lambda: afile.read(CHUNKSIZE), b''):
+            hasher = hashlib.sha1(cur_chunk) #hash the file
+            fingerprint = hasher.hexdigest() #fingerprint is a hash value
+            check_duplicate = fingerprint in hash_table #check if this fingerprint in hash_table
+            if(check_duplicate == False):
+                hash_table[fingerprint] = cur_chunk #insert it into dictionary
+                unique_size_in_BYTE += len(cur_chunk)
+            total_size_in_BYTE += len(cur_chunk)
+            output_file.writelines(fingerprint + '\n') #write fp into output file
+            afile.seek(CHUNKSIZE, 1) #move to next chunk
 
-                    hasher = hashlib.sha1(buf) #hash the file
-                    fingerprint = hasher.hexdigest() #fingerprint is a hash value
-
-                    check_duplicate = fingerprint in hash_table #check if this fingerprint in hash_table
-
-                    #file_size = os.path.getsize(filename) #get the size of each file
-                    #print file_size
-                    if(check_duplicate == False):
-                        hash_table[fingerprint] = filename #insert it into dictionary
-                        #unique += file_size
-                        unique += len(buf)
-                    #total += file_size
-                    total += len(buf)
-                    
-                    #if(hasher.hexdigest() == '193b92bdedac1894a3ffc56d7b7fd308afb00d63' or hasher.hexdigest() == '19a73ebc32c1e076eac457cf1f9c8d6dde6e8c30'):
-                        #print hasher.hexdigest()
-                    buf = afile.read(BLOCKSIZE)
-                    count += 1
-
-
-show_dedupe_rate(total, unique)
+show_dedupe_rate(total_size_in_BYTE, unique_size_in_BYTE)
 
